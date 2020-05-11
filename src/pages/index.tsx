ff1e85001a1984fc8f5834b22936c50ed6cd4c11
jianbin.liao <jianbin.liao@aseit.cn>
2020-05-11 15:38:47 +0800

    恰什莫

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,73 +2,7 @@ import React, { useState } from 'react';
 import styles from './index.css';
 import Card from '@/components/card/Card';
 // import ChessBoard from '@/components/chessboard';
-
-const data = [
-  {
-    key: 0,
-    name: '虾小哥',
-    x: Math.random() * 800 - 400,
-    y: Math.random() * 50 - 25,
-    z: Math.round(Math.random() * 90),
-    deg: Math.random() * 180,
-  },
-  {
-    key: 1,
-    name: '铁碗饭',
-    x: Math.random() * 800 - 400,
-    y: Math.random() * 50 - 25,
-    z: Math.round(Math.random() * 90),
-    deg: Math.random() * 180,
-  },
-  {
-    key: 2,
-    name: '陕老顺',
-    x: Math.random() * 800 - 400,
-    y: Math.random() * 50 - 25,
-    z: Math.round(Math.random() * 90),
-    deg: Math.random() * 180,
-  },
-  {
-    key: 3,
-    name: '胖妈水饺',
-    x: Math.random() * 800 - 400,
-    y: Math.random() * 50 - 25,
-    z: Math.round(Math.random() * 90),
-    deg: Math.random() * 180,
-  },
-  {
-    key: 4,
-    name: '聚光食堂',
-    x: Math.random() * 800 - 400,
-    y: Math.random() * 50 - 25,
-    z: Math.round(Math.random() * 90),
-    deg: Math.random() * 180,
-  },
-  {
-    key: 5,
-    name: '迷上你',
-    x: Math.random() * 800 - 400,
-    y: Math.random() * 50 - 25,
-    z: Math.round(Math.random() * 90),
-    deg: Math.random() * 180,
-  },
-  {
-    key: 6,
-    name: '望江面馆',
-    x: Math.random() * 800 - 400,
-    y: Math.random() * 50 - 25,
-    z: Math.round(Math.random() * 90),
-    deg: Math.random() * 180,
-  },
-  {
-    key: 7,
-    name: '大鱼',
-    x: Math.random() * 800 - 400,
-    y: Math.random() * 50 - 25,
-    z: Math.round(Math.random() * 90),
-    deg: Math.random() * 180,
-  },
-];
+import { data } from './config';
 
 export default function() {
   const [current, setCurrent] = useState<number | null>(null);
